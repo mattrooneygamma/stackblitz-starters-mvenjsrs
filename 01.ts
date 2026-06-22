72c687277f2093cf5a7692c0f13f9e33e1e28893
mattrooneygamma <matt.rooney@gamma.co.uk>
2026-06-22 12:47:39 +0100

    Refactor getLargestNumber with todo comment
    Refactor getLargestNumber function to include a todo comment.

--- 01.ts
+++ 01.ts
@@ -3,6 +3,5 @@
  * @returns {number|undefined} Largest number, or undefined if array is empty
  */
 export const getLargestNumber = (array) => {
-  if (!array.length) return undefined;
-  return Math.max(...array);
+//todo
 };
